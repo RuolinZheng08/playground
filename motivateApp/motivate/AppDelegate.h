c9aa2fd3ec1541e4cd6a56ca5461b6c9d6f413fd
Ruolin Zheng <zheng_ruolin@hotmail.com>
2019-08-19 23:02:15 +0800

    Change from command line tool to .app

---
+++ motivateApp/motivate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  motivate
+//
+//  Created by Ruolin Zheng on 2019/8/19.
+//  Copyright © 2019 Ruolin Zheng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ motivateApp/motivate/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  motivate
+//
+//  Created by Ruolin Zheng on 2019/8/19.
+//  Copyright © 2019 Ruolin Zheng. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
